a061b48f9fa884e417936f13c67f5b7ad7235dd0
Duduumendez <nicksoniaudax5@gmail.com>
2024-07-14 13:27:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.location="Lahore,Pakistan."
 
 
 global.mongodb= process.env.MONGODB_URI || ""
-global.allowJids= process.env.ALLOW_JID || "null" 
+global.allowJids= process.env.ALLOW_JID || "nulla" 
 global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
 
